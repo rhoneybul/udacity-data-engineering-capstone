e9c7983452e5363e0ac0a6e5d7dd3fbe2140e805
Robert Honeybul <honeybulr@gmail.com>
2019-08-24 15:00:21 +0800

    added DAG w/ data validations

--- dags/create_tables_dag.py
+++ dags/create_tables_dag.py
@@ -4,6 +4,8 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 import pandas as pd
 from operators.create_tables import CreateTableOperator
+from operators.ensure_records import EnsureRecords
+from operators.ensure_distinct import EnsureDistinctRecords
 from operators.etl import ETLOperator
 from operators.create_dimensions import DimensionTableOperator
 from operators.helpers.read_dataframes import read_immigration_data, read_global_temperatures, read_global_temperatures_by_country, read_demographics, read_airport_codes
@@ -123,15 +125,63 @@ create_cities = CreateTableOperator(task_id='create_cities',
 
 create_airport_codes = CreateTableOperator(task_id='create_airport_codes',
                                            table_name='airport_codes',
-                                           dag=dag)                             
+                                           dag=dag)           
 
-start_operator >> create_immigration >> etl_immigration_data 
-start_operator >> create_global_temperatures >> etl_global_temperatures
-start_operator >> create_global_temperatures_by_country >> etl_global_temperatures_by_country
-start_operator >> create_demographics >> etl_demographics
-start_operator >> create_airport_codes >> etl_airport_codes
-start_operator >> create_countries >> create_countries_dimension_table
-start_operator >> create_cities >> create_cities_dimension_table
+check_immigration_records = EnsureRecords(task_id='ensure_immigration_recoreds',
+                                          table_name='immigration',
+                                          dag=dag)
+
+check_global_temperature_records = EnsureRecords(task_id='ensure_global_temperature_records',
+                                                 table_name='global_temperatures',
+                                                 dag=dag)
+
+check_global_temperature_by_country_records = EnsureRecords(task_id='ensure_global_temperature_by_country_records',
+                                                            table_name='global_temperature_by_country_records',
+                                                            dag=dag)
+
+check_country_records = EnsureRecords(task_id='ensure_country_records',
+                                      table_name='countries',
+                                      dag=dag)
+
+check_demographic_records = EnsureRecords(task_id='check_demographic_records',
+                                          table_name='demographics',
+                                          dag=dag)
+
+check_cities = EnsureRecords(task_id='check_cities_records',
+                             table_name='cities',
+                             dag=dag)
+
+check_airport_codes = EnsureRecords(task_id='check_aiport_codes',
+                                    table_name='airport_codes',
+                                    dag=dag)
+
+check_immigration_distinct_records = EnsureDistinctRecords(task_id='check_distinct_immigration_records',
+                                                           table_name='immigrations',
+                                                           distinct_column='ins_number',
+                                                           dag=dag)
+
+check_cities_distinct_records = EnsureDistinctRecords(task_id='check_distinct_city_records',
+                                                      table_name='cities',
+                                                      distinct_column='name',
+                                                      dag=dag)     
+
+check_countries_distinct_records = EnsureDistinctRecords(task_id='check_distinct_country_records',
+                                                         table_name='countries',
+                                                         distinct_column='name',
+                                                         dag=dag)
+
+check_airport_codes_distinct_records = EnsureDistinctRecords(task_id='check_airport_codes_distinct_records',
+                                                             table_name='airport_codes',
+                                                             distinct_column='id',
+                                                             dag=dag)                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+
+start_operator >> create_immigration >> etl_immigration_data >> check_immigration_records >> check_immigration_distinct_records
+start_operator >> create_global_temperatures >> etl_global_temperatures >> check_global_temperature_records
+start_operator >> create_global_temperatures_by_country >> etl_global_temperatures_by_country >> check_global_temperature_by_country_records
+start_operator >> create_demographics >> etl_demographics 
+start_operator >> create_airport_codes >> etl_airport_codes >> check_airport_codes >> check_airport_codes_distinct_records
+start_operator >> create_countries >> create_countries_dimension_table >> check_country_records >> check_countries_distinct_records
+start_operator >> create_cities >> create_cities_dimension_table >> check_cities >> check_cities_distinct_records
 
 
 etl_global_temperatures >> create_countries_dimension_table
